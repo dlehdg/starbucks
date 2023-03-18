e127881049a6eecfca1c251aac5d93f87e6f73b2
dlehdg <ldg2409@naver.com>
2023-03-18 23:12:52 +0900

    순차적 애니메이션 추가

--- js/main.js
+++ js/main.js
@@ -18,6 +18,32 @@ searchInputEl.addEventListener('blur', function() {
 
 const badgeEl = document.querySelector('header .badges');
 
-window.addEventListener('scroll', function () {
-  console.log('scroll');
-})
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if(window.scrollY > 500) {
+      //배지 숨기기
+      gsap.to(badgeEl, .6, { // 자바스크립트 애니메이션 라이브러리
+        opacity: 0,
+        display: 'none'
+      }); // gsap.to(요소, 지속시간, 옵션) 시간이 지나면 보이게 구현
+  }
+  else {
+       //배지 보이기
+      gsap.to(badgeEl, .6, {
+        opacity: 1,
+        display: 'block'
+      });
+  }
+}, 300));
+
+// _.throttle(함수, 시간)
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  // gsap.to(요소, 지속시간(초단위), 옵션(객체))
+  gsap.to(fadeEl, 1, {
+    delay:(index + 1) * .7, // 지연 -> 순차적으로 0.7초 마다 화면에 나오도록 지현시킴
+    opacity: 1
+  });
+}); // 요소들의 개수만큼 반복
