80bd58c14a32cb1efb08fba8c98c507f7a2c173c
dlehdg <ldg2409@naver.com>
2023-04-03 20:32:11 +0900

    요소슬라이드 완

--- js/main.js
+++ js/main.js
@@ -47,3 +47,44 @@ fadeEls.forEach(function (fadeEl, index) {
     opacity: 1
   });
 }); // 요소들의 개수만큼 반복
+
+
+new Swiper('.notice-line .swiper-container', {  // new Swiper(요소, 옵션) 생성자
+  direction: 'vertical', // 방향 -> 수직
+  autoplay: true, // 자동 슬라이드 재생
+  loop: true, // 반복 재생
+});
+
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3,  // 한번에 보여주는 슬라이드 개수
+  spaceBetween: 10,  // 술라이드 사이 여백
+  centeredSlides: true,  // 1번 슬라이드가 가운데에 보이기  
+  loop: true,
+  autoplay: {
+    delay: 5000, // 0.5 초 마다 슬라이드
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true, // 사용자의 페이지 번호 요소 제어 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev', // 이전 버튼
+    nextEl: '.promotion .swiper-next' // 다음 버튼
+  }
+}); 
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', () => {
+  isHidePromotion = !isHidePromotion;
+  if(isHidePromotion){
+    // 숨김 처리
+    promotionEl.classList.add('hide');
+  }
+  else {
+    // 보임 처리
+    promotionEl.classList.remove('hide');
+  }
+});
